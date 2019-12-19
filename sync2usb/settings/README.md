43099c2d49f594e9ab9b0d0d04f8cc531ed8ea26
Thomas Harold <tgharold@gmail.com>
2019-12-19 15:32:09 -0500

    mucking about with some skeleton code

---
+++ sync2usb/settings/README.md
@@ -0,0 +1,8 @@
+# Sync2USB / Settings
+
+## Sources
+
+- https://realpython.com/command-line-interfaces-python-argparse/
+- https://www.python.org/dev/peps/pep-0389/
+- https://bitbucket.org/tkhyn/dirsync/src/default/dirsync/options.py
+- https://www.devdungeon.com/content/python-configuration-files
--- sync2usb/settings/settings.py
+++ sync2usb/settings/settings.py
@@ -1,6 +1,31 @@
-import os
+#import os
 import sys
 from argparse import ArgumentParser
-from configparser import ConfigParser
-from collections import OrderedDict
+#from configparser import ConfigParser
+#from collections import OrderedDict
 
+
+class ArgParser(ArgumentParser):
+
+    def __init__(self, *args, **kwargs):
+        return
+
+    def parse_args(self, args=None, namespace=None):
+        if args or len(sys.argv) > 1:
+            # if no args nor sys.argv, we don't bother loading the config as
+            # there are missing args and super().parse_args below will raise an
+            # exception
+            self.load_cfg(args[0] if args else sys.argv[1])
+
+        parsed = super(ArgParser, self).parse_args(args, namespace)
+
+        if parsed.action is False:
+            raise ValueError('Argument error: you must select an action using '
+                             'one of the "sync", "update" or "diff" options\n')
+
+        return parsed        
+
+    def load_cfg(self, src_dir):
+        return
+
+    
